f58e1ef2a0e2d43853def819a929aa3971b06b0b
Bogdan Todorov <90504727+bockoman@users.noreply.github.com>
2022-08-20 15:57:05 +0200

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let modal = document.querySelector('.modal');
+let showModal = document.querySelectorAll('.show-modal');
+let closeModal = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openOrClose);
+}
+
+overlay.addEventListener('click', openOrClose);
+
+closeModal.addEventListener('click', openOrClose);
+
+function openOrClose() {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+}
+
+document.addEventListener('keydown', function (e) {
+  let keyCode = e.keyCode;
+  if (keyCode === 27) openOrClose();
+});
